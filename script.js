49ec6bf03f4e852f9cbae94b7910d31ddd5318c1
LucasGrataoPe12 <108188844+LucasGrataoPe12@users.noreply.github.com>
2023-06-14 15:44:17 -0300

    upup

---
+++ script.js
@@ -0,0 +1,18 @@
+let fruta = 'fruta'
+
+let span = document.getElementById('texto')
+fruta = prompt('O que deseja?')
+
+switch (fruta) {
+    case 'maçã':
+        alert('Não vendemos essa fruta aqui')
+        break
+    case 'kiwi':
+        alert('Estamos com escassez de kiwis')
+        break
+    case 'melancia':
+        alert('Aqui está, são 3 reais o quilo')
+        break
+    default:
+        alert("Recarregue a página e experimento novos resultados")
+}
